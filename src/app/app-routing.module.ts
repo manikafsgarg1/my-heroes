72f0f91dce6b8652737486daefcdac233341494c
manikafsgarg1 <manikafsgarg@gmail.com>
2018-07-09 17:41:27 +0530

    Routers

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
